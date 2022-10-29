5989635dc9ce230ceeed79a6a66e2924773a9737
jarrodmjack <jarrodmjack@gmail.com>
2022-10-29 19:09:06 -0400

    created frontend with Next.js

---
+++ 02_front/frontend/pages/descriptionpage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const descriptionpage = () => {
+  return (
+    <div>descriptionpage</div>
+  )
+}
+
+export default descriptionpage
---
+++ 02_front/frontend/pages/listpage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const listpage = () => {
+  return (
+    <div>listpage</div>
+  )
+}
+
+export default listpage
